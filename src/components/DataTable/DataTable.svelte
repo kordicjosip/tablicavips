ff01ffff4beb93bd40eed5db01fb8c0b2cbb2d5f
Josip <josip.kordic6@gmail.com>
2023-04-28 11:55:28 +0200

    nova slika i data.json

--- src/components/DataTable/DataTable.svelte
+++ src/components/DataTable/DataTable.svelte
@@ -145,7 +145,7 @@
 </script>
 
 <svg height="100%" id="table" width="100%">
-  <image href="/slika.png" width="1548" height="1472" class=""></image>
+  <image href="/slika.png" width="1548" height="1472" transform="translate({X0} {Y0})"></image>
   <g transform="translate({X} 0)">
     {#each data.columns as column}
       <ColumnHeader bind:column onRightClick={showContextMenu} />
