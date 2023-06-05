16577f3911f6a54df1142c523699f9c5f2d88024
Josip <josip.kordic6@gmail.com>
2023-06-05 15:21:08 +0200

    dodaj dialog za spremanje predloška stupaca

---
+++ src/components/VipsTableOCR/Dialog.svelte
@@ -0,0 +1,37 @@
+<script lang="ts" xmlns="http://www.w3.org/1999/html">
+	let dialog;
+
+	export function show() {
+		dialog.showModal();
+	}
+
+	function save() {
+		closeModal();
+	}
+
+	const closeModal = () => {
+		dialog.close();
+	};
+</script>
+
+<dialog bind:this={dialog} class="bg-neutral-700 text-center w-fit rounded">
+	<div id="dialogdiv">
+		<label class="text-white" for="name">Unesite naziv predloška:</label><br />
+		<input type="text" id="name" name="name" required class="px-1 mt-3 mb-5" minlength="1" />
+		<br />
+		<div class="flex justify-between w-full">
+			<button class="h-8 text-white px-2 py-0.5 items-center" on:click={closeModal}>Odustani</button
+			>
+			<button
+				class="rounded-[0.2rem] h-8 font-medium text-white outline outline-1 px-2 py-0.5 items-center"
+				on:click={save}>Spremi</button
+			>
+		</div>
+	</div>
+</dialog>
+
+<style>
+	dialog::backdrop {
+		background: rgba(0, 0, 0, 0.5);
+	}
+</style>
--- src/components/VipsTableOCR/Navbar.svelte
+++ src/components/VipsTableOCR/Navbar.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
 	import { createEventDispatcher } from 'svelte';
+	import Dialog from './Dialog.svelte';
 
+	let dialog;
 	const dispatch = createEventDispatcher();
 	export let scale: number;
 	export let numberOfPages: number | null;
@@ -16,13 +18,14 @@
 	{#if numberOfPages > 0}
 		<div>
 			<input
+				class={isUnlinked ? 'cursor-default' : 'cursor-pointer'}
 				type="checkbox"
-				id="uredi stranicu"
+				id="otkači stranicu"
 				bind:checked={isUnlinked}
 				on:change={() => dispatch('toggleUnlink', isUnlinked)}
 				name="zaključaj"
 			/>
-			<label for="uredi stranicu" class="text-base">Otkači ovu stranicu</label>
+			<label for="otkači stranicu" class="text-base">Otkači ovu stranicu</label>
 		</div>
 		<div class="flex items-center">
 			<button
@@ -40,16 +43,51 @@
 
 		<div class="flex items-center">
 			<button
-				class="w-7 h-7 hover:bg-neutral-600 rounded-xl m-1"
+				class="w-7 h-7 hover:bg-neutral-600 rounded-full m-1"
 				on:click={() => (scale = Math.max(scale - 0.05, 0.01))}
 				>-
 			</button>
 			<div class="select-none">{scalePercent}%</div>
 			<button
-				class="w-7 h-7 hover:bg-neutral-600 rounded-xl m-1"
+				class="w-7 h-7 hover:bg-neutral-600 rounded-full m-1"
 				on:click={() => (scale = Math.min(scale + 0.05, 2.5))}
 				>+
 			</button>
 		</div>
+
+		<div
+			title="Spremi stupce kao predložak"
+			class="flex items-center justify-center w-7 h-7 rounded-full hover:bg-neutral-600 hover:cursor-pointer"
+			on:click={dialog.show}
+		>
+			<svg viewBox="-2.4 -2.4 28.80 28.80" fill="none" xmlns="http://www.w3.org/2000/svg">
+				<g clip-path="url(#clip0_429_11181)">
+					<path
+						d="M7 21C5.89543 21 5 20.1046 5 19V3H14L19 8V19C19 20.1046 18.1046 21 17 21H7Z"
+						stroke="white"
+						stroke-width="1.2"
+						stroke-linecap="round"
+						stroke-linejoin="round"
+					/>
+					<path
+						d="M12 13V17"
+						stroke="white"
+						stroke-width="1.2"
+						stroke-linecap="round"
+						stroke-linejoin="round"
+					/>
+					<path
+						d="M10 15H14"
+						stroke="white"
+						stroke-width="1.2"
+						stroke-linecap="round"
+						stroke-linejoin="round"
+					/>
+					<path d="M13 3V9H19" stroke="white" stroke-width="1.2" stroke-linejoin="round" />
+				</g>
+			</svg>
+		</div>
 	{/if}
 </div>
+
+<Dialog bind:this={dialog} />
