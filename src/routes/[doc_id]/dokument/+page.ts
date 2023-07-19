2cb22c66aa2af6f0ebe072c079e12e6d574e8459
Josip <josip.kordic6@gmail.com>
2023-07-19 11:02:08 +0200

    uvijek postavi data

--- src/routes/[doc_id]/dokument/+page.ts
+++ src/routes/[doc_id]/dokument/+page.ts
@@ -13,20 +13,29 @@ export const load: PageLoad = async ({ params, fetch }) => {
 	if (browser) {
 		table = JSON.parse(localStorage.getItem(params.doc_id)!);
 		for (const columnIndex in table.columns) {
-			if (table.columns[columnIndex].field === Field.artiklPoSifri) {
-				for (const row of table.tablica) {
-					row.cells[columnIndex].data = getArtiklPoSifri(row.cells[columnIndex]?.text | '', fetch);
-				}
-				await Promise.all(
-					table.tablica.map(
-						async (row) => (row.cells[columnIndex].data = await row.cells[columnIndex].data)
-					)
-				);
-			}
-			if (table.columns[columnIndex].field === Field.numeric) {
-				for (const row of table.tablica) {
-					row.cells[columnIndex].data = validateInputNumeric(row.cells[columnIndex]?.text);
-				}
+			switch (table.columns[columnIndex].field) {
+				case Field.artiklPoSifri:
+					for (const row of table.tablica) {
+						row.cells[columnIndex].data = getArtiklPoSifri(
+							row.cells[columnIndex]?.text | '',
+							fetch
+						);
+					}
+					await Promise.all(
+						table.tablica.map(
+							async (row) => (row.cells[columnIndex].data = await row.cells[columnIndex].data)
+						)
+					);
+					break;
+				case Field.numeric:
+					for (const row of table.tablica) {
+						row.cells[columnIndex].data = validateInputNumeric(row.cells[columnIndex]?.text);
+					}
+					break;
+				case Field.text:
+					for (const row of table.tablica) {
+						row.cells[columnIndex].data = row.cells[columnIndex]?.text;
+					}
 			}
 		}
 		console.log(table);
