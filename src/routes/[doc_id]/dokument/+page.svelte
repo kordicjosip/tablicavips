cef8d8498c317e0b2b81592a71efbefeb7539c9d
Josip <josip.kordic6@gmail.com>
2023-07-28 13:37:31 +0200

    ipak centriraj tablicu

--- src/routes/[doc_id]/dokument/+page.svelte
+++ src/routes/[doc_id]/dokument/+page.svelte
@@ -239,238 +239,243 @@
 	{/if}
 </div>
 
-<table>
-	<thead class="select-none sticky -top-[1px]">
-		<tr>
-			{#each data.table.columns as header}
-				{#if header.field === Field.artiklPoSifri}
-					<th colspan="2">Šifra</th>
-				{:else if header.field === Field.artiklPoKataloskomBroju}
-					<th colspan="2">Kataloški broj</th>
-				{:else if header.field === Field.artiklPoBarKodu}
-					<th colspan="2">Barkod</th>
-				{:else}
-					<th>{header.name}</th>
-				{/if}
-			{/each}
-			{#if vipsDocument}
-				<th>
-					<button class="block m-auto" title="Upiši sve moguće redove" on:click={sendAllRowsData}>
-						<svg
-							class="send"
-							height="22px"
-							viewBox="0 0 24 24"
-							width="22px"
-							xmlns="http://www.w3.org/2000/svg"
-						>
-							<g id="SVGRepo_bgCarrier" stroke-width="0" />
-							<g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round" />
-							<g id="SVGRepo_iconCarrier">
-								<defs>
-									<style>
-										.cls-1 {
-											fill: none;
-											stroke: #020202;
-											stroke-miterlimit: 10;
-											stroke-width: 1.91px;
-										}
-									</style>
-								</defs>
-								<path
-									class="cls-1"
-									d="M1.5,18.68H8.18A3.82,3.82,0,0,1,12,22.5v0a0,0,0,0,1,0,0H1.5a0,0,0,0,1,0,0V18.68A0,0,0,0,1,1.5,18.68Z"
-								/>
-								<path
-									class="cls-1"
-									d="M15.82,18.68H22.5a0,0,0,0,1,0,0V22.5a0,0,0,0,1,0,0H12a0,0,0,0,1,0,0v0A3.82,3.82,0,0,1,15.82,18.68Z"
-								/>
-								<path
-									class="cls-1"
-									d="M1.5,6.27H8.18A3.82,3.82,0,0,1,12,10.09V22.5a0,0,0,0,1,0,0H1.5a0,0,0,0,1,0,0V6.27A0,0,0,0,1,1.5,6.27Z"
-								/>
-								<path class="cls-1" d="M19.64,6.27H22.5V22.5H12V10.09a3.82,3.82,0,0,1,3.82-3.82" />
-								<path
-									class="cls-1"
-									d="M19.64,13l-1.91,1.91L15.82,13V3.41A1.9,1.9,0,0,1,17.73,1.5h0a1.91,1.91,0,0,1,1.91,1.91Z"
-								/>
-							</g>
-						</svg>
-					</button>
-				</th>
-			{/if}
-		</tr>
-	</thead>
-	<tbody>
-		{#each data.table.tablica as row}
+<div class="flex justify-center">
+	<table>
+		<thead class="select-none sticky -top-[1px]">
 			<tr>
-				{#each row.cells as cell, i (cell.colParam + cell.rowNumber)}
-					{#if !row.disabled}
-						{#if data.table.columns[i].field === Field.artiklPoSifri}
-							<td
-								on:focusin={() => {
-									setOCRPreviewData(cell);
-									OCRPreviewVisible = true;
-								}}
-								on:focusout={() => (OCRPreviewVisible = false)}
-								><input
-									size="14"
-									class="bg-transparent"
-									class:bg-red-300={cell.text === ''}
-									type="text"
-									bind:value={cell.text}
-									on:input={async () => {
-										cell.data = await getArtiklPoSifri(cell.text, fetch);
-									}}
-								/>
-							</td>
-							<td class="text-gray-700 select-none" class:bg-red-300={!cell.data}
-								>{cell.data ? cell.data.Naziv : 'Ne postoji'}</td
-							>
-						{:else if data.table.columns[i].field === Field.artiklPoKataloskomBroju}
-							<td
-								on:focusin={() => {
-									setOCRPreviewData(cell);
-									OCRPreviewVisible = true;
-								}}
-								on:focusout={() => (OCRPreviewVisible = false)}
-								><input
-									size="14"
-									class="bg-transparent"
-									class:bg-red-300={cell.text === ''}
-									type="text"
-									bind:value={cell.text}
-									on:input={async () => {
-										cell.data = await getArtiklPoKataloskomBroju(cell.text, fetch);
-									}}
-								/>
-							</td>
-							<td class="text-gray-700" class:bg-red-300={!cell.data}
-								>{cell.data ? cell.data.Naziv : 'Ne postoji'}</td
-							>
-						{:else if data.table.columns[i].field === Field.artiklPoBarKodu}
-							<td
-								on:focusin={() => {
-									setOCRPreviewData(cell);
-									OCRPreviewVisible = true;
-								}}
-								on:focusout={() => (OCRPreviewVisible = false)}
-								><input
-									size="14"
-									class="bg-transparent"
-									class:bg-red-300={cell.text === ''}
-									type="text"
-									bind:value={cell.text}
-									on:input={async () => {
-										cell.data = await getArtiklPoBarKodu(cell.text, fetch);
-									}}
-								/>
-							</td>
-							<td class="text-gray-700 select-none" class:bg-red-300={!cell.data}
-								>{cell.data ? cell.data.Naziv : 'Ne postoji'}</td
-							>
-						{:else if data.table.columns[i].field === Field.numeric}
-							<td
-								on:focusin={() => {
-									setOCRPreviewData(cell);
-									OCRPreviewVisible = true;
-								}}
-								on:focusout={() => (OCRPreviewVisible = false)}
-								class:bg-red-300={cell.data === null}
-								class:bg-yellow-300={cell.text === '' &&
-									data.table.columns[i].defaultValue !== null}
-								><input
-									size="14"
-									class="bg-transparent"
-									type="text"
-									bind:value={cell.text}
-									on:input={async () => {
-										cell.data = validateInputNumeric(cell.text);
-									}}
-								/>
-							</td>
-						{:else}
-							<td
-								on:focusin={() => {
-									setOCRPreviewData(cell);
-									OCRPreviewVisible = true;
-								}}
-								on:focusout={() => (OCRPreviewVisible = false)}
-								class:bg-green-300={row.disabled}
-								class:bg-yellow-300={cell.text === '' &&
-									data.table.columns[i].defaultValue !== null}
-								class:bg-red-300={cell.text === ''}
-								contenteditable
-								bind:textContent={cell.text}>{cell.text}</td
-							>
-						{/if}
-					{:else if data.table.columns[i].field === Field.artiklPoSifri}
-						<td class:bg-green-300={row.disabled}>{cell.text}</td>
-						<td class="text-gray-700 select-none" class:bg-green-300={row.disabled}
-							>{cell.data ? cell.data.Naziv : 'Ne postoji'}</td
-						>
-					{:else if data.table.columns[i].field === Field.artiklPoKataloskomBroju}
-						<td class:bg-green-300={row.disabled}>{cell.text}</td>
-						<td class="text-gray-700 select-none" class:bg-green-300={row.disabled}
-							>{cell.data ? cell.data.Naziv : 'Ne postoji'}</td
-						>
-					{:else if data.table.columns[i].field === Field.numeric}
-						<td class:bg-green-300={row.disabled}>{cell.text}</td>
+				{#each data.table.columns as header}
+					{#if header.field === Field.artiklPoSifri}
+						<th colspan="2">Šifra</th>
+					{:else if header.field === Field.artiklPoKataloskomBroju}
+						<th colspan="2">Kataloški broj</th>
+					{:else if header.field === Field.artiklPoBarKodu}
+						<th colspan="2">Barkod</th>
 					{:else}
-						<td class:bg-green-300={row.disabled}>{cell.text}</td>
+						<th>{header.name}</th>
 					{/if}
 				{/each}
 				{#if vipsDocument}
-					<td class:bg-green-300={row.disabled}>
-						{#if row.disabled}
-							<button class="block m-auto" disabled>
-								<svg
-									class="sent"
-									width="24px"
-									height="24px"
-									viewBox="0 0 24 24"
-									fill="none"
-									xmlns="http://www.w3.org/2000/svg"
-								>
-									<g id="SVGRepo_bgCarrier" stroke-width="0" />
-									<g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round" />
-									<g id="SVGRepo_iconCarrier">
-										<path
-											fill-rule="evenodd"
-											clip-rule="evenodd"
-											d="M20.6097 5.20743C21.0475 5.54416 21.1294 6.17201 20.7926 6.60976L10.7926 19.6098C10.6172 19.8378 10.352 19.9793 10.0648 19.9979C9.77765 20.0166 9.49637 19.9106 9.29289 19.7072L4.29289 14.7072C3.90237 14.3166 3.90237 13.6835 4.29289 13.2929C4.68342 12.9024 5.31658 12.9024 5.70711 13.2929L9.90178 17.4876L19.2074 5.39034C19.5441 4.95258 20.172 4.87069 20.6097 5.20743Z"
-											fill="#000000"
-										/>
-									</g>
-								</svg>
-							</button>
-						{:else}
-							<button title="Spremi red" class="block m-auto" on:click={() => sendRowData(row)}>
-								<svg
-									class="send"
-									width="24px"
-									height="24px"
-									viewBox="0 0 24 24"
-									xmlns="http://www.w3.org/2000/svg"
-								>
-									<g id="SVGRepo_bgCarrier" stroke-width="0" />
-									<g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round" />
-									<g id="SVGRepo_iconCarrier">
-										<path
-											fill-rule="evenodd"
-											clip-rule="evenodd"
-											d="M19.186 2.09c.521.25 1.136.612 1.625 1.101.49.49.852 1.104 1.1 1.625.313.654.11 1.408-.401 1.92l-7.214 7.213c-.31.31-.688.541-1.105.675l-4.222 1.353a.75.75 0 0 1-.943-.944l1.353-4.221a2.75 2.75 0 0 1 .674-1.105l7.214-7.214c.512-.512 1.266-.714 1.92-.402zm.211 2.516a3.608 3.608 0 0 0-.828-.586l-6.994 6.994a1.002 1.002 0 0 0-.178.241L9.9 14.102l2.846-1.496c.09-.047.171-.107.242-.178l6.994-6.994a3.61 3.61 0 0 0-.586-.828zM4.999 5.5A.5.5 0 0 1 5.47 5l5.53.005a1 1 0 0 0 0-2L5.5 3A2.5 2.5 0 0 0 3 5.5v12.577c0 .76.082 1.185.319 1.627.224.419.558.754.977.978.442.236.866.318 1.627.318h12.154c.76 0 1.185-.082 1.627-.318.42-.224.754-.559.978-.978.236-.442.318-.866.318-1.627V13a1 1 0 1 0-2 0v5.077c0 .459-.021.571-.082.684a.364.364 0 0 1-.157.157c-.113.06-.225.082-.684.082H5.923c-.459 0-.57-.022-.684-.082a.363.363 0 0 1-.157-.157c-.06-.113-.082-.225-.082-.684V5.5z"
-											fill="#000000"
-										/>
-									</g>
-								</svg>
-							</button>
-						{/if}
-					</td>
+					<th>
+						<button class="block m-auto" title="Upiši sve moguće redove" on:click={sendAllRowsData}>
+							<svg
+								class="send"
+								height="22px"
+								viewBox="0 0 24 24"
+								width="22px"
+								xmlns="http://www.w3.org/2000/svg"
+							>
+								<g id="SVGRepo_bgCarrier" stroke-width="0" />
+								<g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round" />
+								<g id="SVGRepo_iconCarrier">
+									<defs>
+										<style>
+											.cls-1 {
+												fill: none;
+												stroke: #020202;
+												stroke-miterlimit: 10;
+												stroke-width: 1.91px;
+											}
+										</style>
+									</defs>
+									<path
+										class="cls-1"
+										d="M1.5,18.68H8.18A3.82,3.82,0,0,1,12,22.5v0a0,0,0,0,1,0,0H1.5a0,0,0,0,1,0,0V18.68A0,0,0,0,1,1.5,18.68Z"
+									/>
+									<path
+										class="cls-1"
+										d="M15.82,18.68H22.5a0,0,0,0,1,0,0V22.5a0,0,0,0,1,0,0H12a0,0,0,0,1,0,0v0A3.82,3.82,0,0,1,15.82,18.68Z"
+									/>
+									<path
+										class="cls-1"
+										d="M1.5,6.27H8.18A3.82,3.82,0,0,1,12,10.09V22.5a0,0,0,0,1,0,0H1.5a0,0,0,0,1,0,0V6.27A0,0,0,0,1,1.5,6.27Z"
+									/>
+									<path
+										class="cls-1"
+										d="M19.64,6.27H22.5V22.5H12V10.09a3.82,3.82,0,0,1,3.82-3.82"
+									/>
+									<path
+										class="cls-1"
+										d="M19.64,13l-1.91,1.91L15.82,13V3.41A1.9,1.9,0,0,1,17.73,1.5h0a1.91,1.91,0,0,1,1.91,1.91Z"
+									/>
+								</g>
+							</svg>
+						</button>
+					</th>
 				{/if}
 			</tr>
-		{/each}
-	</tbody>
-</table>
+		</thead>
+		<tbody>
+			{#each data.table.tablica as row}
+				<tr>
+					{#each row.cells as cell, i (cell.colParam + cell.rowNumber)}
+						{#if !row.disabled}
+							{#if data.table.columns[i].field === Field.artiklPoSifri}
+								<td
+									on:focusin={() => {
+										setOCRPreviewData(cell);
+										OCRPreviewVisible = true;
+									}}
+									on:focusout={() => (OCRPreviewVisible = false)}
+									><input
+										size="14"
+										class="bg-transparent"
+										class:bg-red-300={cell.text === ''}
+										type="text"
+										bind:value={cell.text}
+										on:input={async () => {
+											cell.data = await getArtiklPoSifri(cell.text, fetch);
+										}}
+									/>
+								</td>
+								<td class="text-gray-700 select-none" class:bg-red-300={!cell.data}
+									>{cell.data ? cell.data.Naziv : 'Ne postoji'}</td
+								>
+							{:else if data.table.columns[i].field === Field.artiklPoKataloskomBroju}
+								<td
+									on:focusin={() => {
+										setOCRPreviewData(cell);
+										OCRPreviewVisible = true;
+									}}
+									on:focusout={() => (OCRPreviewVisible = false)}
+									><input
+										size="14"
+										class="bg-transparent"
+										class:bg-red-300={cell.text === ''}
+										type="text"
+										bind:value={cell.text}
+										on:input={async () => {
+											cell.data = await getArtiklPoKataloskomBroju(cell.text, fetch);
+										}}
+									/>
+								</td>
+								<td class="text-gray-700" class:bg-red-300={!cell.data}
+									>{cell.data ? cell.data.Naziv : 'Ne postoji'}</td
+								>
+							{:else if data.table.columns[i].field === Field.artiklPoBarKodu}
+								<td
+									on:focusin={() => {
+										setOCRPreviewData(cell);
+										OCRPreviewVisible = true;
+									}}
+									on:focusout={() => (OCRPreviewVisible = false)}
+									><input
+										size="14"
+										class="bg-transparent"
+										class:bg-red-300={cell.text === ''}
+										type="text"
+										bind:value={cell.text}
+										on:input={async () => {
+											cell.data = await getArtiklPoBarKodu(cell.text, fetch);
+										}}
+									/>
+								</td>
+								<td class="text-gray-700 select-none" class:bg-red-300={!cell.data}
+									>{cell.data ? cell.data.Naziv : 'Ne postoji'}</td
+								>
+							{:else if data.table.columns[i].field === Field.numeric}
+								<td
+									on:focusin={() => {
+										setOCRPreviewData(cell);
+										OCRPreviewVisible = true;
+									}}
+									on:focusout={() => (OCRPreviewVisible = false)}
+									class:bg-red-300={cell.data === null}
+									class:bg-yellow-300={cell.text === '' &&
+										data.table.columns[i].defaultValue !== null}
+									><input
+										size="14"
+										class="bg-transparent"
+										type="text"
+										bind:value={cell.text}
+										on:input={async () => {
+											cell.data = validateInputNumeric(cell.text);
+										}}
+									/>
+								</td>
+							{:else}
+								<td
+									on:focusin={() => {
+										setOCRPreviewData(cell);
+										OCRPreviewVisible = true;
+									}}
+									on:focusout={() => (OCRPreviewVisible = false)}
+									class:bg-green-300={row.disabled}
+									class:bg-yellow-300={cell.text === '' &&
+										data.table.columns[i].defaultValue !== null}
+									class:bg-red-300={cell.text === ''}
+									contenteditable
+									bind:textContent={cell.text}>{cell.text}</td
+								>
+							{/if}
+						{:else if data.table.columns[i].field === Field.artiklPoSifri}
+							<td class:bg-green-300={row.disabled}>{cell.text}</td>
+							<td class="text-gray-700 select-none" class:bg-green-300={row.disabled}
+								>{cell.data ? cell.data.Naziv : 'Ne postoji'}</td
+							>
+						{:else if data.table.columns[i].field === Field.artiklPoKataloskomBroju}
+							<td class:bg-green-300={row.disabled}>{cell.text}</td>
+							<td class="text-gray-700 select-none" class:bg-green-300={row.disabled}
+								>{cell.data ? cell.data.Naziv : 'Ne postoji'}</td
+							>
+						{:else if data.table.columns[i].field === Field.numeric}
+							<td class:bg-green-300={row.disabled}>{cell.text}</td>
+						{:else}
+							<td class:bg-green-300={row.disabled}>{cell.text}</td>
+						{/if}
+					{/each}
+					{#if vipsDocument}
+						<td class:bg-green-300={row.disabled}>
+							{#if row.disabled}
+								<button class="block m-auto" disabled>
+									<svg
+										class="sent"
+										width="24px"
+										height="24px"
+										viewBox="0 0 24 24"
+										fill="none"
+										xmlns="http://www.w3.org/2000/svg"
+									>
+										<g id="SVGRepo_bgCarrier" stroke-width="0" />
+										<g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round" />
+										<g id="SVGRepo_iconCarrier">
+											<path
+												fill-rule="evenodd"
+												clip-rule="evenodd"
+												d="M20.6097 5.20743C21.0475 5.54416 21.1294 6.17201 20.7926 6.60976L10.7926 19.6098C10.6172 19.8378 10.352 19.9793 10.0648 19.9979C9.77765 20.0166 9.49637 19.9106 9.29289 19.7072L4.29289 14.7072C3.90237 14.3166 3.90237 13.6835 4.29289 13.2929C4.68342 12.9024 5.31658 12.9024 5.70711 13.2929L9.90178 17.4876L19.2074 5.39034C19.5441 4.95258 20.172 4.87069 20.6097 5.20743Z"
+												fill="#000000"
+											/>
+										</g>
+									</svg>
+								</button>
+							{:else}
+								<button title="Spremi red" class="block m-auto" on:click={() => sendRowData(row)}>
+									<svg
+										class="send"
+										width="24px"
+										height="24px"
+										viewBox="0 0 24 24"
+										xmlns="http://www.w3.org/2000/svg"
+									>
+										<g id="SVGRepo_bgCarrier" stroke-width="0" />
+										<g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round" />
+										<g id="SVGRepo_iconCarrier">
+											<path
+												fill-rule="evenodd"
+												clip-rule="evenodd"
+												d="M19.186 2.09c.521.25 1.136.612 1.625 1.101.49.49.852 1.104 1.1 1.625.313.654.11 1.408-.401 1.92l-7.214 7.213c-.31.31-.688.541-1.105.675l-4.222 1.353a.75.75 0 0 1-.943-.944l1.353-4.221a2.75 2.75 0 0 1 .674-1.105l7.214-7.214c.512-.512 1.266-.714 1.92-.402zm.211 2.516a3.608 3.608 0 0 0-.828-.586l-6.994 6.994a1.002 1.002 0 0 0-.178.241L9.9 14.102l2.846-1.496c.09-.047.171-.107.242-.178l6.994-6.994a3.61 3.61 0 0 0-.586-.828zM4.999 5.5A.5.5 0 0 1 5.47 5l5.53.005a1 1 0 0 0 0-2L5.5 3A2.5 2.5 0 0 0 3 5.5v12.577c0 .76.082 1.185.319 1.627.224.419.558.754.977.978.442.236.866.318 1.627.318h12.154c.76 0 1.185-.082 1.627-.318.42-.224.754-.559.978-.978.236-.442.318-.866.318-1.627V13a1 1 0 1 0-2 0v5.077c0 .459-.021.571-.082.684a.364.364 0 0 1-.157.157c-.113.06-.225.082-.684.082H5.923c-.459 0-.57-.022-.684-.082a.363.363 0 0 1-.157-.157c-.06-.113-.082-.225-.082-.684V5.5z"
+												fill="#000000"
+											/>
+										</g>
+									</svg>
+								</button>
+							{/if}
+						</td>
+					{/if}
+				</tr>
+			{/each}
+		</tbody>
+	</table>
+</div>
 {#if data.table.tablica[0] && OCRPreviewData.stranica !== undefined && OCRPreviewVisible}
 	<OCRPreview
 		image={data.documentData['stranice'][OCRPreviewData.stranica].tablica['definicija'].image}
